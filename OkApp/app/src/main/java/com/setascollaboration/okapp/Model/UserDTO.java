d6e312d84303570ba3c01a6fa9d250ee296d674a
Tri Nguyen <nmtri.itc@gmail.com>
2020-03-14 19:20:01 -0400

    Established API to interact with server

---
+++ OkApp/app/src/main/java/com/setascollaboration/okapp/Model/UserDTO.java
@@ -0,0 +1,31 @@
+package com.setascollaboration.okapp.Model;
+
+public class UserDTO {
+    private String userName;
+    private String password;
+    private String token;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
