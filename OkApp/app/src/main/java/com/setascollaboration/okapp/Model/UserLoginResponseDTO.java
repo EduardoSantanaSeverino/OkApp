0636fcde31ae806da858033fa5bdebc91186e6e2
Tri Nguyen <nmtri.itc@gmail.com>
2020-03-30 19:42:55 -0400

    fixed API call in login implementing google map (60%)

--- OkApp/app/src/main/java/com/setascollaboration/okapp/Model/UserLoginResponseDTO.java
+++ OkApp/app/src/main/java/com/setascollaboration/okapp/Model/UserLoginResponseDTO.java
@@ -5,17 +5,15 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserLoginResponseDTO {
 
+    private UserLoginResultDTO result;
 
-        private UserLoginResultDTO result;
+    public UserLoginResultDTO getResult() {
+        return result;
+    }
 
-        public UserLoginResultDTO getResult() {
-            return result;
-        }
-
-        public void setResult(UserLoginResultDTO result) {
-            this.result = result;
-        }
-
+    public void setResult(UserLoginResultDTO result) {
+        this.result = result;
+    }
 
 
 }
--- OkApp/app/src/main/java/com/setascollaboration/okapp/ui/home/HomeViewModel.java
+++ OkApp/app/src/main/java/com/setascollaboration/okapp/ui/home/HomeViewModel.java
@@ -1,19 +1,18 @@
 package com.setascollaboration.okapp.ui.home;
 
+import android.content.Intent;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+import com.google.android.gms.maps.GoogleMap;
 
-    private MutableLiveData<String> mText;
+public class HomeViewModel extends ViewModel {
+    private GoogleMap mMap;
 
     public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+
     }
 
-    public LiveData<String> getText() {
-        return mText;
-    }
 }
